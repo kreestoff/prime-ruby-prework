55abd6327b8792d4f54f5aa3a30bcbb128302430
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-15 20:27:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(number)
   if number >= 2
-    (2..number - 1).all? do |x|
+    highest_denominator = number - 1
+    (2..highest_denominator).all? do |x|
       number % x != 0
     end
   else
